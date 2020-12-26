53a3758dccaf48bf239cc8a004c0ca19ac7001c9
박준호 <junho6667@gmail.com>
2020-12-26 21:01:47 +0900

    wow

---
+++ src/main/java/junho/whiteship/Moim.java
@@ -0,0 +1,21 @@
+package junho.whiteship;
+
+/**
+ * Hello world!
+ *
+ */
+public class Moim {
+    int maxNumberOfAttendees;
+    int numberOfEnrollment;
+
+    public boolean isErollmentFull(){
+        if(maxNumberOfAttendees ==0){
+            return false;
+        }
+        if (numberOfEnrollment < maxNumberOfAttendees){
+            return false;
+        }
+        return true;
+
+    }
+}
---
+++ src/test/java/junho/whiteship/MoimTest.java
@@ -0,0 +1,17 @@
+package junho.whiteship;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoimTest {
+    @Test
+    public void isFull(){
+        Moim moim = new Moim();
+        moim.maxNumberOfAttendees = 100;
+        moim.numberOfEnrollment = 10;
+
+        Assert.assertFalse(moim.isErollmentFull());
+    }
+
+}
